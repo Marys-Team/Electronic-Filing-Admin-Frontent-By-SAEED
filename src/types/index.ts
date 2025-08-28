029b0e40af915c7525de30ca6d5cb517f13ce94b
BadarNawazIREG <faculty2.cust@gmail.com>
2025-08-28 12:48:32 +0500

    3rd commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,15 +13,3 @@ interface FormField {
   justification: string;
   description: string;
 }
-
-interface RecordDefinition {
-  name: string;
-  indicator: string;
-  indicatorType: string;
-  length: string;
-  position: string;
-  repeat: boolean;
-  repeatCount?: string;
-  fieldCount: string;
-  fields: FormField[];
-}
